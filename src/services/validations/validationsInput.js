763099eac3351083097955c9a505544ea10e17bf
Gabriel Galera <gabgalera@hotmail.com>
2023-02-13 18:16:00 -0300

    Arruma o 3

--- src/services/validations/validationsInput.js
+++ src/services/validations/validationsInput.js
@@ -4,7 +4,7 @@ const validateName = (name) => {
   const { error } = nameSchema.validate(name);
 
   let errorType = 500;
-  if (error.message === '"value" length must be at least 5 characters long') {
+  if (error && error.message === '"value" length must be at least 5 characters long') {
     errorType = 422;
   } else {
     errorType = 400;
