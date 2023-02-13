49ad779c536d8e7aafd641d45141d036f2d8692b
Gabriel Galera <gabgalera@hotmail.com>
2023-02-13 15:52:14 -0300

    Cria  a primeira rota

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const connection = require('./models/connection');
 
 const app = express();
+
+app.get('/products', async (req, res) => {
+  const [allProducts] = await connection.execute(
+    'SELECT * FROM StoreManager.products',
+  );
+
+  res.status(200).json(allProducts);
+});
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
