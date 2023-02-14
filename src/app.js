e170b96c8260358f660bfebdf04fd21763c1ccb7
Gabriel Galera <gabgalera@hotmail.com>
2023-02-14 13:06:36 -0300

    Testando no avaliador do github

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { productsController, salesController } = require('./controllers/index');
+const connection = require('./models/connection');
 
 const app = express();
 
@@ -7,9 +8,26 @@ app.use(express.json());
 
 app.post('/sales', salesController.newSale);
 
-app.post('/products', productsController.insert);
+app.get('/sales/:id', async (req, res) => {
+  const { id } = req.params;
+  const [sale] = await connection.execute(
+    'SELECT * FROM sales WHERE id = ?',
+    [id],
+  );
+  if (!sale) return res.status(404).json({ message: 'Sale not found' });
+  res.status(200).json(sale);
+});
+
+app.get('/sales', async (_req, res) => {
+  const [allSales] = await connection.execute(
+    'SELECT * FROM sales',
+  );
+  res.status(200).json(allSales);
+});
 
 app.get('/products/:id', productsController.findById);
+
+app.post('/products', productsController.insert);
 
 app.get('/products', productsController.allProducts);
 
